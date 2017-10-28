6cc6c387a7999b5c9f9013d86225d28e9f953a08
TheRealGiovas <e1_giovas@hotmail.com>
2017-10-28 09:46:40 -0500

    Add files via upload

---
+++ Proyect_Home/home.js
@@ -0,0 +1,143 @@
+$('document').ready(function(){
+   $('.pop').hide();
+});
+
+
+                //perfil
+$("#perfil").mouseenter(function(){
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#perfilR").show();
+    $("#perfilR").dialog();
+    
+});
+
+
+$("#perfil").mouseleave(function(){
+          document.body.style.backgroundColor = "white";
+     $("#perfilR").hide();
+    
+});
+
+
+                //personalidad
+$("#personalidad").mouseenter(function(){
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#personalidadR").show();
+    $("#personalidadR").dialog();
+    
+});
+
+
+$("#personalidad").mouseleave(function(){
+          document.body.style.backgroundColor = "white";
+     $("#personalidadR").hide();
+    
+});
+
+                //referentes
+$("#referentes").mouseenter(function(){
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#referentesR").show();
+    $("#referentesR").dialog();
+    
+});
+
+
+$("#referentes").mouseleave(function(){
+          document.body.style.backgroundColor = "white";
+     $("#referentesR").hide();
+    
+});
+
+                //experience goals
+$("#experience").mouseenter(function(){
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#experienceR").show();
+    $("#experienceR").dialog();
+    
+});
+
+
+$("#experience").mouseleave(function(){
+          document.body.style.backgroundColor = "white";
+     $("#experienceR").hide();
+    
+});
+
+                //plataformas
+$("#platform").mouseenter(function(){
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#platformR").show();
+    $("#platformR").dialog();
+    
+});
+
+
+$("#platform").mouseleave(function(){
+          document.body.style.backgroundColor = "white";
+     $("#platformR").hide();
+    
+});
+
+
+                //must do
+$("#mustD").mouseenter(function(){
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#mustDR").show();
+    $("#mustDR").dialog();
+    
+});
+
+
+$("#mustD").mouseleave(function(){
+          document.body.style.backgroundColor = "white";
+     $("#mustDR").hide();
+    
+});
+
+                //must never
+$("#must").mouseenter(function(){
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#mustR").show();
+    $("#mustR").dialog();
+     
+    
+});
+
+
+$("#must").mouseleave(function(){
+      document.body.style.backgroundColor = "white";
+     $("#mustR").hide();
+    
+});
+
+            //apps
+$("#apps").mouseenter(function(){
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#appsR").show();
+    $("#appsR").dialog();
+    
+});
+
+
+$("#apps").mouseleave(function(){
+          document.body.style.backgroundColor = "white";
+     $("#appsR").hide();
+    
+});
+
+
+            //marca
+$("#marca").mouseenter(function(){
+      document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+     $("#marcaR").show();
+    $("#marcaR").dialog();
+    
+});
+
+
+$("#marca").mouseleave(function(){
+          document.body.style.backgroundColor = "white";
+     $("#marcaR").hide();
+    
+});
